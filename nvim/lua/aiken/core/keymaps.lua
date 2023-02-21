53328089ab727b3eda1971103e7c62a938be37b1
aikenji <67273613+aikenji@users.noreply.github.com>
2023-02-21 12:10:37 +0800

    add nvim pulgin telescope

--- nvim/lua/aiken/core/keymaps.lua
+++ nvim/lua/aiken/core/keymaps.lua
@@ -18,3 +18,11 @@ keymap.set("n", "x", '"_x')
 
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>lds", "<cmd>Telescope lsp_document_symbols<cr>") -- list all functions/structs/classes/modules in the current buffer
--- nvim/lua/aiken/plugins/plugins-setup.lua
+++ nvim/lua/aiken/plugins/plugins-setup.lua
@@ -43,6 +43,10 @@ return require('packer').startup(function(use)
   -- statusline
   use 'nvim-lualine/lualine.nvim'
 
+   -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
   -- auto closing
   use 'windwp/nvim-autopairs' 
 
