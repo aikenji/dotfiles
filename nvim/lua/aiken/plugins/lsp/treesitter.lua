c1d567f546474c1d6b9d45ee9c1e5658746b8a61
aikenji <67273613+aikenji@users.noreply.github.com>
2023-03-03 22:07:02 +0800

    20230302

--- nvim/lua/aiken/plugins/lsp/treesitter.lua
+++ nvim/lua/aiken/plugins/lsp/treesitter.lua
@@ -1,36 +1,36 @@
 local M = {}
 
 function M.init()
-  -- import nvim-treesitter plugin safely
-  local status, treesitter = pcall(require, "nvim-treesitter.configs")
-  if not status then
-    return
-  end
+    -- import nvim-treesitter plugin safely
+    local status, treesitter = pcall(require, "nvim-treesitter.configs")
+    if not status then
+        return
+    end
 
-  -- configure treesitter
-  treesitter.setup({
-    -- enable syntax highlighting
-    highlight = {
-      enable = true,
-    },
-    -- enable indentation
-    -- indent = { enable = true, disable = { "python" } },
-    -- enable autotagging (w/ nvim-ts-autotag plugin)
-    autotag = { enable = true },
-    -- ensure these language parsers are installed
-    ensure_installed = {
-      "c",
-      "cpp",
-      "markdown",
-      "markdown_inline",
-      "bash",
-      "lua",
-      "vim",
-      "gitignore",
-    },
-    -- auto install above language parsers
-    auto_install = true,
-  })
+    -- configure treesitter
+    treesitter.setup({
+        -- enable syntax highlighting
+        highlight = {
+            enable = true,
+        },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        -- autotag = { enable = true },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+            "c",
+            "cpp",
+            "cmake",
+            "python",
+            "markdown",
+            "markdown_inline",
+            "bash",
+            "lua",
+            "vim",
+            "gitignore",
+        },
+        -- auto install above language parsers
+        auto_install = true,
+    })
 end
 
 return M
