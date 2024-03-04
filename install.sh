e256771da8c1e9384fe675cd9353c282d1093c6e
aikenji <aikenjch@gmail.com>
2024-03-04 19:52:30 +0800

    add yabai and skhd setup

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 set -e
 
 TERMINALS="kitty/ alacritty/"
+WM="yabai/ skhd/"
 BASICS="git/ zsh/ conda/"
 TOOLS="tmux/ nvim/ lazygit/ joshuto/"
 
-DOT_FOLDERS="$TERMINALS $BASICS $TOOLS"
+DOT_FOLDERS="$TERMINALS $WM $BASICS $TOOLS"
 
 for folder in $DOT_FOLDERS
 do
