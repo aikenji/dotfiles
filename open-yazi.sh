52fdb1828604570eb5ca6e11b0b2fbadf83e36b8
aikenji <aikenjch@gmail.com>
2025-04-28 17:55:18 +0800

    2025-04-28

--- open-yazi.sh
+++ open-yazi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Applications/kitty.app/Contents/MacOS/kitty -- /bin/zsh -i -c 'yazi'
+/Applications/kitty.app/Contents/MacOS/kitty -o macos_quit_when_last_window_closed=yes -o remember_window_size=no -o initial_window_width=1200 -o initial_window_height=800 -- /bin/zsh -i -c 'yazi'
