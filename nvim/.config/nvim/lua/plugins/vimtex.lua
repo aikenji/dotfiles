42a6aa5c18c09d628cea54b7baf862da635dfea6
aikenji <aikenjch@gmail.com>
2025-05-03 12:25:35 +0800

    add dropbar to replace barbecue

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -10,7 +10,7 @@ return {
       vim.g.tex_indent_items = 0
       vim.g.vimtex_fold_enabled = 1
       vim.g.vimtex_format_enabled = 1
-      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_view_method = "sioyek"
 
       -- Don't open QuickFix for warning messages if no errors are present
       vim.g.vimtex_quickfix_open_on_warning = 0
