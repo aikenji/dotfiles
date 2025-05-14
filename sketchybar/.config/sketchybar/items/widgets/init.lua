b7bc749cfff8a78a4fdcb14cd6d15977772d6305
aikenji <aikenjch@gmail.com>
2025-05-14 11:22:23 +0800

    add battery on sbar

--- sketchybar/.config/sketchybar/items/widgets/init.lua
+++ sketchybar/.config/sketchybar/items/widgets/init.lua
@@ -1,7 +1,7 @@
 -- require "items.widgets.messages"
 require "items.widgets.volume"
 require "items.widgets.wifi"
--- require "items.widgets.battery"
+require "items.widgets.battery"
 
 sbar.add("bracket", { "/widgets\\..*/" }, {})
 
