1f6fd5f59ffe689c3c6ec3c6dd771b812b61fb7c
aikenji <aikenjch@gmail.com>
2025-05-19 11:14:19 +0800

    add mini.surround

--- nvim/.config/nvim/lua/plugins/pairs.lua
+++ nvim/.config/nvim/lua/plugins/pairs.lua
@@ -4,4 +4,21 @@ return {
     version = "*",
     opts = {},
   },
+
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
+  },
 }
