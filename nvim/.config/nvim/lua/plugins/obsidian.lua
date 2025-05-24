33ea97b9f61501830c6b3373b6490ac4149adf90
aikenji <aikenjch@gmail.com>
2025-05-24 14:52:38 +0800

    reconfig obsidian.nvim

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,88 @@
+return {
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "second-brain",
+            path = "~/Notes/second-brain/",
+          },
+        },
+
+        -- notes_subdir = "notes",
+        new_notes_location = "notes_subdir",
+
+        note_frontmatter_func = function(note)
+          local out = { id = note.id, aliases = note.aliases, date = os.date("%Y-%m-%d"), tags = note.tags }
+
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+          end
+
+          return out
+        end,
+        daily_notes = {
+          folder = "dailies",
+          date_format = "%Y-%m-%d",
+          alias_format = "%B %-d, %Y",
+          default_tags = { "daily-notes" },
+          template = nil,
+          workdays_only = true,
+        },
+
+        note_id_func = function(title)
+          return title
+        end,
+
+        disable_frontmatter = false,
+        ui = {
+          enable = false,
+          checkboxes = {
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+          },
+        },
+        completion = {
+          nvim_cmp = false,
+          blink = true,
+        },
+        picker = {
+          -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+          name = "snacks.pick",
+          note_mappings = {
+            -- Create a new note from your query.
+            new = "<C-x>",
+            -- Insert a link to the selected note.
+            insert_link = "<C-l>",
+          },
+          tag_mappings = {
+            -- Add tag(s) to current note.
+            tag_note = "<C-x>",
+            -- Insert a tag at the current location.
+            insert_tag = "<C-l>",
+          },
+        },
+        statusline = {
+          enabled = true,
+          format = "{{properties}} properties {{backlinks}} backlinks",
+        },
+      })
+      vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+      vim.keymap.set("n", "<leader>oO", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
+      vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
+      vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
+      vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+      vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
+      vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
+    end,
+  },
+}
