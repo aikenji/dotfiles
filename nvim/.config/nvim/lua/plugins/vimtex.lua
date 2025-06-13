ed97a5cef0e8312c9fc449fae952bdda298379ba
aikenji <aikenjch@gmail.com>
2025-06-13 16:50:05 +0800

    nvim/adjust some setting in vimtex

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -4,12 +4,12 @@ return {
     "lervag/vimtex",
     init = function()
       vim.opt.conceallevel = 2
-      vim.g.vimtex_complete_enabled = 0
+      vim.g.vimtex_complete_enabled = 1
       vim.g.vimtex_imaps_enabled = 0
       vim.g.vimtex_indent_enabled = 0
       vim.g.tex_indent_items = 0
-      vim.g.vimtex_fold_enabled = 1
-      vim.g.vimtex_format_enabled = 1
+      vim.g.vimtex_fold_enabled = 0
+      vim.g.vimtex_format_enabled = 0
       vim.g.vimtex_view_method = "sioyek"
 
       -- Don't open QuickFix for warning messages if no errors are present
